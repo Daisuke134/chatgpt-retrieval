4829391da4c87e0062aeb33ef33781d70b292b18
Daisuke134 <140220114+Daisuke134@users.noreply.github.com>
2023-07-30 06:54:48 +0900

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
-APIKEY = "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"
+APIKEY = "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"
